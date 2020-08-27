725d9430a4907b21c7a5d5166153f21e3d648832
vito <vitohong@126.com>
2020-08-27 22:48:31 +0800

    Vito 初始化 0827晚

---
+++ src/huawei/HJ2_CountCharacterNumber.java
@@ -0,0 +1,51 @@
+package huawei;
+
+/*
+题目描述
+写出一个程序，接受一个由字母和数字组成的字符串，和一个字符，然后输出输入字符串中含有该字符的个数。不区分大小写。
+输入描述:
+第一行输入一个有字母和数字以及空格组成的字符串，第二行输入一个字符。
+输出描述:
+输出输入字符串中含有该字符的个数。
+示例1
+输入
+ABCDEF
+A
+输出
+1
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * 思路：
+ * 使用hashmap能在O(1)时间内找到对应字符出现的次数，
+ * 整个算法时间复杂度O(n),空间复杂度O(n).
+ * 能不能时间更短？
+ *
+ * 我傻了。。
+ * 直接遍历一遍字符串不就行了
+ */
+public class HJ2_CountCharacterNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String string = sc.nextLine();
+        char ch = sc.next().charAt(0);
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<string.length();i++){
+            char temp = string.charAt(i);
+            if(temp>='A'&&temp<='Z'){
+                temp = Character.toLowerCase(temp);
+            }
+            if(map.get(temp)==null){
+                map.put(temp,1);
+            } else {
+                map.put(temp,map.get(temp)+1);
+            }
+        }
+        Integer num = map.get(ch);
+        System.out.println(num==null?0:num);
+    }
+}
---
+++ src/huawei/HJ4_SplitString.java
@@ -0,0 +1,67 @@
+package huawei;
+
+/*
+题目描述
+•连续输入字符串，请按长度为8拆分每个字符串后输出到新的字符串数组；
+•长度不是8整数倍的字符串请在后面补数字0，空字符串不处理。
+输入描述:
+连续输入字符串(输入2次,每个字符串长度小于100)
+输出描述:
+输出到长度为8的新字符串数组
+示例1
+输入
+abc
+123456789
+输出
+abc00000
+12345678
+90000000
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 思路：
+ * 按行读取字符串，然后将字符串切分。
+ * 只输入两次，所以只要创建最大数组为ceil(100/8)*2即可。
+ */
+public class HJ4_SplitString {
+    public static void main(String[] args) throws IOException {
+        int length = 2*(100/8)+2;
+        String[] strings = new String[length];
+        int index = 0;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line1 = br.readLine();
+        String line2 = br.readLine();
+
+
+        for(int i=0;i<line1.length()/8;i++){
+            strings[index] = line1.substring(8*index,8*index+8);
+            index++;
+        }
+        int temp = line1.length()%8;
+        int start = line1.length()-temp;
+        if(temp!=0){
+            strings[index] = "00000000";
+            strings[index] = line1.substring(start)+strings[index].substring(temp);
+            index++;
+        }
+        for(int i=0;i<line2.length()/8;i++){
+            strings[index] = line2.substring(8*i,8*i+8);
+            index++;
+        }
+        temp = line2.length()%8;
+        start = line2.length()-temp;
+        if(temp!=0){
+            strings[index] = "00000000";
+            strings[index] = line2.substring(start)+strings[index].substring(temp);
+            index++;
+        }
+        for(int i=0;i<index;i++){
+            System.out.println(strings[i]);
+        }
+    }
+}
